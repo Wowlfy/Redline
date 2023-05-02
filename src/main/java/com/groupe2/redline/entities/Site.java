1ce067035beaf4c2d767269e6b4696d9e8783181
Verdad <xavier.ristat@gmail.com>
2023-05-02 15:54:14 +0200

    entities Utilisateur + Site

---
+++ src/main/java/com/groupe2/redline/entities/Site.java
@@ -0,0 +1,64 @@
+package com.groupe2.redline.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "site")
+public class Site {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "libelle")
+    private String libelle;
+
+    @Column(name = "adresse")
+    private String adresse;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "actif")
+    private boolean actif;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActif() {
+        return actif;
+    }
+
+    public void setActif(boolean actif) {
+        this.actif = actif;
+    }
+}
--- src/main/java/com/groupe2/redline/entities/Utilisateur.java
+++ src/main/java/com/groupe2/redline/entities/Utilisateur.java
@@ -1,8 +1,6 @@
 package com.groupe2.redline.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 
@@ -10,8 +8,9 @@ import java.io.Serializable;
 @Table(name = "utilisateur")
 public class Utilisateur implements Serializable {
 
-    @Column(name = "id")
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(name = "nom")
     private String nom;
@@ -21,4 +20,36 @@ public class Utilisateur implements Serializable {
 
     @Column(name = "mot_de_passe")
     private String mot_de_passe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMot_de_passe() {
+        return mot_de_passe;
+    }
+
+    public void setMot_de_passe(String mot_de_passe) {
+        this.mot_de_passe = mot_de_passe;
+    }
 }
