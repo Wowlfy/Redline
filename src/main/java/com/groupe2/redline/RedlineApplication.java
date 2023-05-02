729e2cf290e2548a223751a16277a9440deb6861
Wowlfy <maxime.giraudeau@hotmail.fr>
2023-05-02 10:38:51 +0200

    First Push

---
+++ src/main/java/com/groupe2/redline/RedlineApplication.java
@@ -0,0 +1,13 @@
+package com.groupe2.redline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupe2/redline/RedlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe2.redline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
