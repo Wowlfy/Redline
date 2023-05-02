4384fdad7cf14898ccab0b650c48b7ee910b95fd
johannweytens <weytensjohann771@gmail.com>
2023-05-02 15:47:06 +0200

    Correction date --> datetime

---
+++ src/main/resources/db/migration/V1_1__INIT_DB.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `demande`
+MODIFY COLUMN `proposition_date_1` datetime NOT NULL,
+MODIFY COLUMN `proposition_date_2` datetime NOT NULL,
+MODIFY COLUMN `proposition_date_3` datetime NOT NULL;
