a8a1a675d6b9bc1ddad14460b11ebc9bfeeeeaa4
Oozogul <onur.o@emiles.fr>
2023-05-03 09:57:05 +0200

    Création des repositories

---
+++ src/main/java/com/groupe2/redline/repository/DemandeRepository.java
@@ -0,0 +1,9 @@
+package com.groupe2.redline.repository;
+
+import com.groupe2.redline.entities.Demande;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DemandeRepository extends JpaRepository<Demande, Long> {
+}
---
+++ src/main/java/com/groupe2/redline/repository/UtilisateurRepository.java
@@ -0,0 +1,9 @@
+package com.groupe2.redline.repository;
+
+import com.groupe2.redline.entities.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+}
