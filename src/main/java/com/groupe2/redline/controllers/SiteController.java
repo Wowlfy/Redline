461238631dc2421261ebb7cafb3f32ff44ae4ed7
Verdad <xavier.ristat@gmail.com>
2023-05-04 12:25:14 +0200

    editSite pour controler et service + doc swagger pour route des sites

--- src/main/java/com/groupe2/redline/controllers/SiteController.java
+++ src/main/java/com/groupe2/redline/controllers/SiteController.java
@@ -2,6 +2,10 @@ package com.groupe2.redline.controllers;
 
 import com.groupe2.redline.entities.Site;
 import com.groupe2.redline.services.SiteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
@@ -27,14 +31,24 @@ public class SiteController {
         return ResponseEntity.ok(sites);
     }
 
+    @Operation(summary = "Ajouter un site")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Site créé"),
+            @ApiResponse(responseCode = "500", description = "Erreur interne")
+    })
     @PostMapping(value = "/add", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity <Site> addSite(@RequestBody Site site) {
         Site saveSite = siteService.addSite(site);
         return new ResponseEntity<>(saveSite, HttpStatus.OK);
     }
 
+    @Operation(summary = "Modifier un site")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Site modifié"),
+            @ApiResponse(responseCode = "500", description = "Erreur interne")
+    })
     @PutMapping(value = "/{id}/edit", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity <Site> updateSite(@PathVariable Long id, @RequestBody Site site) throws ClassNotFoundException {
+    public ResponseEntity <Site> updateSite(@PathVariable Long id, @RequestBody Site site) throws EntityNotFoundException {
         return new ResponseEntity<>(siteService.editSite(id, site), HttpStatus.OK);
     }
 }
--- src/main/java/com/groupe2/redline/services/SiteService.java
+++ src/main/java/com/groupe2/redline/services/SiteService.java
@@ -2,6 +2,7 @@ package com.groupe2.redline.services;
 
 import com.groupe2.redline.entities.Site;
 import com.groupe2.redline.repository.SiteRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Sort;
@@ -31,16 +32,26 @@ public class SiteService {
         return savedSite;
     }
 
-    public Site editSite(Long id, Site site) throws ClassNotFoundException {
+    public Site editSite(Long id, Site site) throws EntityNotFoundException {
         Optional<Site> editingSite = siteRepository.findById(id);
         if (!editingSite.isPresent()) {
-            throw new ClassNotFoundException("Le site avec l'ID " + id + " n'existe pas.");
+            throw new EntityNotFoundException("Le site avec l'ID " + id + " n'existe pas.");
         }
 
         Site editedSite = editingSite.get();
-        editedSite.setLibelle(site.getLibelle());
-        editedSite.setDescription(site.getDescription());
-        editedSite.setAdresse(site.getAdresse());
+
+        if(site.getLibelle() != null) {
+            editedSite.setLibelle(site.getLibelle());
+
+        }
+        if(site.getDescription() != null) {
+            editedSite.setDescription(site.getDescription());
+
+        }
+
+        if(site.getAdresse() != null) {
+            editedSite.setAdresse(site.getAdresse());
+        }
 
         Site savedSite = siteRepository.save(editedSite);
         return savedSite;
