4811d797f7ea2dbb5bcd63f09e724855947caf94
Axel Choteau <axelc37@outlook.fr>
2023-05-04 14:44:03 +0200

    Site : Activation et désactivation

--- src/main/java/com/groupe2/redline/controllers/SiteController.java
+++ src/main/java/com/groupe2/redline/controllers/SiteController.java
@@ -1,13 +1,13 @@
 package com.groupe2.redline.controllers;
 
 import com.groupe2.redline.entities.Site;
+import com.groupe2.redline.exceptions.SiteDejaActifException;
+import com.groupe2.redline.exceptions.SiteDejaInactifException;
 import com.groupe2.redline.services.SiteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.persistence.EntityNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +25,7 @@ public class SiteController {
         this.siteService = siteService;
     }
 
-    @GetMapping(value ="/get", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/get", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Site>> getSites() {
         List<Site> sites = this.siteService.getAllSites();
         return ResponseEntity.ok(sites);
@@ -37,7 +37,7 @@ public class SiteController {
             @ApiResponse(responseCode = "500", description = "Erreur interne")
     })
     @PostMapping(value = "/add", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity <Site> addSite(@RequestBody Site site) {
+    public ResponseEntity<Site> addSite(@RequestBody Site site) {
         Site saveSite = siteService.addSite(site);
         return new ResponseEntity<>(saveSite, HttpStatus.OK);
     }
@@ -47,8 +47,32 @@ public class SiteController {
             @ApiResponse(responseCode = "200", description = "Site modifié"),
             @ApiResponse(responseCode = "500", description = "Erreur interne")
     })
-    @PutMapping(value = "/{id}/edit", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity <Site> updateSite(@PathVariable Long id, @RequestBody Site site) throws EntityNotFoundException {
+    @PutMapping(value = "/get/{id}/edit", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Site> updateSite(@PathVariable Long id, @RequestBody Site site) throws EntityNotFoundException {
         return new ResponseEntity<>(siteService.editSite(id, site), HttpStatus.OK);
+    }
+
+    @PatchMapping("/get/{id}/activer")
+    public ResponseEntity<String> activer(@PathVariable Long id) {
+        try {
+            siteService.activer(id);
+            return ResponseEntity.status(200).body("Le site a été activé.");
+        } catch (SiteDejaActifException e) {
+            return ResponseEntity.status(409).body("Le site est déjà actif.");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(404).body("Le site spécifié n'existe pas.");
+        }
+    }
+
+    @PatchMapping("/get/{id}/desactiver")
+    public ResponseEntity<String> desactiver(@PathVariable Long id) {
+        try {
+            siteService.desactiver(id);
+            return ResponseEntity.status(200).body("Le site a été désactivé.");
+        } catch (SiteDejaInactifException e) {
+            return ResponseEntity.status(409).body("Le site est déjà inactif.");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(404).body("Le site spécifié n'existe pas.");
+        }
     }
 }
