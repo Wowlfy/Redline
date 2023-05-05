e79cb402ca442c35b37ebe33b425081b18ce0679
Axel Choteau <axelc37@outlook.fr>
2023-05-05 12:25:34 +0200

    Correction contraintes validation SalleDto

--- src/main/java/com/groupe2/redline/dto/SalleDto.java
+++ src/main/java/com/groupe2/redline/dto/SalleDto.java
@@ -1,6 +1,7 @@
 package com.groupe2.redline.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -12,9 +13,11 @@ public class SalleDto {
     @NotBlank
     @Size(max = 255)
     private String description;
-    @NotBlank
+    @NotNull
     @Positive
     private Integer nbPlaces;
+
+    @NotNull
     private Long siteId;
 
 
