15770a8045f8cf2e1488174d36b24645662399a6
Axel Choteau <axelc37@outlook.fr>
2023-05-31 15:20:13 +0200

    Commentaire explicatif sur la requête jpql

--- src/main/java/com/groupe2/redline/repositories/SalleRepository.java
+++ src/main/java/com/groupe2/redline/repositories/SalleRepository.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Repository
 public interface SalleRepository extends JpaRepository<Salle, Long> {
 
+    // CAST(: as date) pour permettre la comparaison avec une date ne stockant que jour/mois/année
     /** Récupérer les salles n'ayant pas de réservation sur cette date + créneau. */
     @Query("SELECT s FROM Salle s WHERE s.site.actif = true AND s.actif = true AND s.id NOT IN (SELECT r.salle.id FROM Reservation r WHERE r.date = CAST(:dateRecherchee AS date) AND r.creneau = :creneauRecherche)")
     List<Salle> rechercherSallesDisponibles(@Param("dateRecherchee") Date dateRecherchee, @Param("creneauRecherche") int creneauRecherche);
